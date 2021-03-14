929dc251d698fe96bdc2687434a171283d81a383
Mateus Luciano <teeusdm@gmail.com>
2021-03-14 08:22:19 -0300

    settings

--- src/app/routers/commentinfo.js
+++ src/app/routers/commentinfo.js
@@ -8,6 +8,6 @@ routes.get('/commentsinfo', CommentInfoController.index)
 routes.get('/commentsinfo/:uuid', validateUuid, CommentInfoController.show)
 routes.post('/commentsinfo', validateData, CommentInfoController.store)
 routes.put('/commentsinfo/:uuid', validateUuid, validateData, CommentInfoController.update)
-routes.delete('/commentsinfo/:uuid', validateUuid, CommentInfoController.delete)
+routes.delete('/commentsinfo/:uuid', /*validateUuid ,*/ CommentInfoController.delete)
 
 export default routes
